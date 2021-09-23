e128006c2742f469667783c4f6e685056921e2bc
HP <shubhi1488@gmail.com>
2021-09-23 16:19:20 +0530

    Initial commit

---
+++ src/Second.java
@@ -0,0 +1,16 @@
+import p1.First;
+import p2.Third;
+
+import java.util.Scanner;
+
+
+public class Second {
+    public static void main(String[] args){
+        First obj= new First();
+        obj.
+        Scanner s=new Scanner(System.in);
+        Third obj2=new Third();
+        Fourth obj3=new Fourth();
+
+    }
+}
---
+++ src/p2/Third.java
@@ -0,0 +1,5 @@
+package p2;
+
+public class Third {
+    public int rollno;
+}
